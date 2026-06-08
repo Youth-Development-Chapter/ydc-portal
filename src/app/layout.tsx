e41ebed1f76f37e98268e3a9a49e96a3a8bf6006
Rohan Ghalib <83542347+RohanGhalib@users.noreply.github.com>
2026-06-08 17:59:39 +0500

    try patch #3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,24 +33,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col relative font-sans">
         {children}
-        <Script 
-          src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" 
-          strategy="afterInteractive"
-        />
-        <Script id="onesignal-init" strategy="afterInteractive">
-          {`
-            window.OneSignalDeferred = window.OneSignalDeferred || [];
-            OneSignalDeferred.push(async function(OneSignal) {
-              await OneSignal.init({
-                appId: "677d8fcd-c6a4-49e0-bea7-22ab8caa8288",
-                safari_web_id: "web.onesignal.auto.63749170-9b18-4e2b-ba12-fbd09a76fb84",
-                notifyButton: {
-                  enable: true,
-                },
-              });
-            });
-          `}
-        </Script>
+
       </body>
     </html>
   );
